b5b75ac2d9b6cfce6cf335ce561aeb28aacf156f
Dan Philibin <dan@danphilibin.com>
2014-08-05 13:44:02 -0400

    added more cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,47 @@
-set :application, 'tunescraper'
+# config valid only for Capistrano 3.1
+lock '3.2.1'
 
+set :application, 'tunescraper'
 set :scm, :git
 set :repo_url, 'git@github.com:danphilibin/tunescraper.git'
-set :branch, "master"
-set :deploy_to, '/var/www/app'
 
-set :ssh_options, {
-  user: "dan",
-  forward_agent: true,
-  port: 22
-}
-
-set :linked_files, %w{config/database.yml config/config.yml}
-set :linked_dirs, %w{bin log tmp vendor/bundle public/system}
-
-set :use_sudo, false
-
-set :rails_env, :production
-
-set :deploy_via, :copy
+set :deploy_to, '/home/deployer/apps/tunescraper'
 
 set :pty, true
 
-set :keep_releases, 5
+set :format, :pretty
 
-server "104.131.207.74", roles: [:app, :web, :db], :primary => true
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
 
 namespace :deploy do
 
-  desc "Restart application"
+  desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join("tmp/restart.txt")
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :finishing, "deploy:cleanup"
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
 
 end
